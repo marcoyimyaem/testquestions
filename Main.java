14caf850568436229eedc860c9b2a81728081fc1
MarcoYimyaem <3720885-MarcoYimyaem@users.noreply.replit.com>
2023-04-25 05:11:55 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,31 @@
+// import java.util.ArrayList;
+// import java.util.*; // for importing java utilities
+// import java.util.function.DoublePredicate;
+// import java.util.*;
+// import java.nio.*; // NO GOOD – a wildcard only matches
+// //class names, not "file.*Files"
+// import java.nio.*.*; // NO GOOD – you can only have one wildcard
+// //and it must be at the end
+// import java.nio.files.Paths.*; // NO GOOD – you cannot import methods
+//only class names
+import java.lang.*;
+public class Main{
+  public Main() {
+    name = "Tiny";
+    System.out.println("setting constructor");
+  }
+   private String name = "Fluffy";
+   { System.out.println("setting field2"); }
+  { System.out.println("setting field"); }
+   
+  public static void main(String[] args) {
+    String beans = "sitaw";
+    beans = "antak";
+    System.out.println(beans); } 
+}
+class Main2{
+  
+}
+ 
+  
+  
---
+++ Person.java
@@ -0,0 +1,3 @@
+class Person{
+  private Person(int x){ }
+	}
